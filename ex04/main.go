aa3cedff941b0690ad73cc822d648fbfd9f6fac3
tatyu <tatyu@t.local>
2024-06-25 20:00:46 +0900

    :seedling:feat: ex04

---
+++ ex04/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"ex04/vender/piscine"
+)
+
+func main() {
+	piscine.PrintComb()
+}
---
+++ ex04/vender/piscine/piscine.go
@@ -0,0 +1,32 @@
+package piscine
+
+import "ex04/vender/ft"
+
+func print(str string) {
+	for _, v := range str {
+		ft.PrintRune(v)
+	}
+}
+
+func createThreeDigitComb() string {
+	var str string
+	for fi := 0; fi <= 7; fi++ {
+		for si := fi + 1; si <= 8; si++ {
+			for ti := si + 1; ti <= 9; ti++ {
+				var newNum string
+				newNum += string(fi + '0')
+				newNum += string(si + '0')
+				newNum += string(ti + '0')
+				if newNum != "789" {
+					newNum += ", "
+				}
+				str += newNum
+			}
+		}
+	}
+	return str
+}
+
+func PrintComb() {
+	print(createThreeDigitComb())
+}
